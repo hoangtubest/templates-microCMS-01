c81420a1c2c907964d375769735c6167171d2a32
hoangtubest <45226294+hoangtubest@users.noreply.github.com>
2023-09-26 11:36:26 +0700

    Update microcms.js

--- assets/js/microcms.js
+++ assets/js/microcms.js
@@ -1,6 +1,8 @@
 export const microcms = {
   // SERVICE_ID: "your-site-name",
   // API_KEY: "your-api-key",
-  SERVICE_ID: "069rmdjxjg",
-  API_KEY: "YzA0dNbLyKO0yw8j4xpCkd4u2w33K2UmL7mq",
+  // SERVICE_ID: "069rmdjxjg",
+  // API_KEY: "YzA0dNbLyKO0yw8j4xpCkd4u2w33K2UmL7mq",
+  SERVICE_ID: "kantan-cms-starter",
+  API_KEY: "92K7xhd4rCrzeCm45dVLvEWke4AXApsFSVvK",
 };
